9ea9893168712788e30c9b0ceba9540af5ae36d1
Vinicius Tomaz <viniciustomax@gmail.com>
2023-07-03 08:10:23 -0400

    fix - tutor routes

--- src/routes/TutorRoutes.ts
+++ src/routes/TutorRoutes.ts
@@ -1,16 +1,15 @@
-import { Router } from "express";
-import {
-    createTutor,
-    getTutors,
-    updateTutorById,
-    deleteTutor
-} from "../controllers/TutorController"
+import express from "express";
+const router = express.Router();
 
-const router = Router();
+import TutorController from "../controllers/TutorController";
+const tutorController = new TutorController();
 
-router.route("/tutor").post(createTutor);
-router.route("/tutors").get(getTutors)
-router.route("/tutor/:id").put(updateTutorById)
-router.route("/tutor/:id").delete(deleteTutor)
+
+router.route("/tutor").post(tutorController.createTutor);
+router.route("/tutors").get(tutorController.getTutors)
+router.route("/tutor/:id").put(tutorController.updateTutor)
+router.route("/tutor/:id").delete(tutorController.deleteTutor)
 
 export default router
+
+
