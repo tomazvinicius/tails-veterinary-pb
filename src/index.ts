402ec4103a0767decdb2ac8a3c1c98b0f4f35989
Vinicius Tomaz <viniciustomax@gmail.com>
2023-07-03 08:11:40 -0400

    fix - index connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
-import TutorRoutes from "./routes/TutorRoutes";
-import PetRoutes from "./routes/PetRoutes";
+require('dotenv').config()
+
+import connectDB from "./db/connect";
 import express from "express";
+import TutorRoutes from "./routes/TutorRoutes";
+// import PetRoutes from "./routes/PetRoutes";
 
 const app = express();
 
 app.use(express.json());
-
 app.use(TutorRoutes);
-app.use(PetRoutes);
+// app.use(PetRoutes);
 
-app.listen(3000, () => console.log("Server listening on port: " + 3000));
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(3000, () => console.log("Server listening on port: " + 3000));
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
